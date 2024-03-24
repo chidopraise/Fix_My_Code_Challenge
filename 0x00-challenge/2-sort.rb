10a907c806c1b8ce6b0e87b2bf82bbd1ac642fce
Abraham Livinus <abrahamlivinus@gmail.com>
2024-03-24 06:34:58 -0700

    2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,19 +1,25 @@
-###
-#
-#  Sort integer arguments (ascending) 
-#
-###
-
 result = []
 ARGV.each do |arg|
     # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+    next unless arg =~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
     
     # insert result at the right position
-    result.insert(result.bsearch_index { |x| x >= i_arg }.to_i || result.size, i_arg)
+    is_inserted = false
+    i = 0
+    l = result.size
+    while i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg unless is_inserted
 end
 
 puts result
